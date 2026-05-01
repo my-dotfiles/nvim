b9d70917dfe699a26c7abb2f49a45938caa8f285
Yurikon <h6606797@gmail.com>
2026-05-01 18:48:12 +0800

    Merge fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Insert 模式下使用 jk 回到 normal 模式
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode" })
