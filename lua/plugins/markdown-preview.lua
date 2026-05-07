fd7e741d2e467098c384348af1b68bce7da8381a
Yurikon <h6606797@gmail.com>
2026-05-07 14:35:06 +0800

    feat: 添加markdow preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,19 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
+    "iamcco/markdown-preview.nvim",
+    cmd = {
+      "MarkdownPreview",
+      "MarkdownPreviewStop",
+      "MarkdownPreviewToggle",
+    },
     ft = { "markdown" },
-    opts = {},
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_open_to_the_world = 0
+      vim.g.mkdp_browser = ""
+    end,
   },
 }
